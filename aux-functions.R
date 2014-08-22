18f4fd45cabc68bf07b6c21b8a4262423517ad4a
gfilippini <g.filippini@hotmail.com>
2014-08-22 00:52:49 -0300

    2o. commit para o peer assignment

--- aux-functions.R
+++ aux-functions.R
@@ -8,6 +8,10 @@ read_stormdata <- function(url,nameout){
     
 }
 
+data<-read.csv("stormdata.csv")
+vars<-c("BGN_DATE","EVTYPE","FATALITIES","INJURIES","PROPDMG","PROPDMGEXP","CROPDMG","CROPDMGEXP")
+data$year<-as.numeric(format(as.Date(data$BGN_DATE,"%m/%d/%Y"),"%Y"))
+
 url1 <- "http://d396qusza40orc.cloudfront.net/repdata%2Fdata%2Factivity.zip"  
 download.file(url1, "activityData.zip", mode="wb") 
 unzip("activity.zip", overwrite=T)
