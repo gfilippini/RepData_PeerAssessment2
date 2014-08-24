d771ff3f8c478c459c60c08e0180d6174aee43be
gfilippini <g.filippini@hotmail.com>
2014-08-24 11:09:38 -0300

    6o. commit - todas as funções e gráficos prontos. Preparação do relatório final.

--- aux-functions.R
+++ aux-functions.R
@@ -21,3 +21,29 @@ a$prop_dmg<-a$PROPDMG*10^a$prop_mult+a$CROPDMG*10^a$crop_mult
 url1 <- 'http://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2'  
 download.file(url1, "activityData.zip", mode="wb") 
 unzip("activity.zip", overwrite=T)
+
+
+totalecon_dmg<-data.frame(tapply(data$econ_dmg, data$year,sum))
+names(totalecon_dmg)<-"t_econ_dmg"
+totalecon_dmg$year<-as.integer(row.names(totalecon_dmg))
+ggplot(totalecon_dmg) + aes(x=year,y=t_econ_dmg) + geom_line(color="blue", size=1)+ expand_limits(x=2000)
+ev_dmg<-data.frame(tapply(data$econ_dmg, as.character(data$EVTYPE), sum,na.rm=T))
+names(ev_dmg)<-"ev_dmg"
+ev_dmg$type<-as.character(row.names(ev_dmg))
+
+ev_dmg <- ev_dmg[order(-ev_dmg$ev_dmg),]
+row.names(ev_dmg)<-NULL
+
+plot_ec_dmg<-ggplot(data = ev_dmg[1:10, ], aes(x = reorder(type,-ev_dmg), y = ev_dmg)) + xlab("Event Type") + ylab("Total Impact")
+
+plot_ec_dmg <- plot_ec_dmg + geom_bar(fill = "darkgreen", stat = "identity") + theme(axis.text.x = element_text(angle = 90, hjust = 0.5, vjust = 1))
+
+hh_dmg<-data.frame(tapply(data$human_inj, as.character(data$EVTYPE), sum,na.rm=T))
+names(hh_dmg)<-"hh_dmg"
+hh_dmg$type<-as.character(row.names(hh_dmg))
+hh_dmg <- hh_dmg[order(-hh_dmg$hh_dmg),]
+
+plot_hh_dmg<-ggplot(data = hh_dmg[1:10, ], aes(x = reorder(type,-hh_dmg), y = hh_dmg)) + xlab("Event Type") + ylab("Total Impact")
+
+plot_hh_dmg <- plot_hh_dmg + geom_bar(fill = "red", stat = "identity") + theme(axis.text.x = element_text(angle = 90, hjust = 0.5, vjust = 1))
+
